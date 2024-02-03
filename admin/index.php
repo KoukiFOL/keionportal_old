1de0274b1837e4dcb6a125eded2c23dd4877ec5e
Kouki_FOL <kohki_ueyama@yahoo.co.jp>
2024-02-03 17:46:00 +0900

    アカウント作成機能（途中）

--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,26 @@
 <?php
 require("../frames/urlpointer.php");
 require("../frames/urlchanger.php");
-require('../frames/header.php'); 
+require('../frames/header.php');
 ?>
 <html>
-    <body>
-    <h2>管理者設定</h2>
-    </body>
+
+<body>
+    <h2>管理者用ページ</h2>
+    <table>
+        <tr>
+            <td><a href="../users/namelist.php">部員管理</a></td>
+        </tr>
+        <tr>
+            <td><a>スタジオ予約（管理者）</a></td>
+        </tr>
+        <tr>
+            <td><a></a></td> 
+        </tr>
+        <tr>
+            <td><a></a></td>
+        </tr>
+    </table>
+</body>
+
 </html>
--- users/login.php
+++ users/login.php
@@ -5,15 +5,37 @@ require('../frames/header.php');
 ?>
 
 <?php
+try{
+    $db = new SQLite3('sqlite:../keionportal.db');
+
+$number = $_POST['number'];
+$password = $_POST['password'];
+
+$stmt = $db -> prepare('select * from users where number = :number');
+$stmt -> bindParam(':number',$number);
+$result = $stmt->execute();
+$user = $result -> fetchArray(SQLITE3_ASSOC); 
+
+if ($user && password_verify($password, $user['password'])) {
+    echo '<p>ログイン成功！</p>';
+    $_SESSION['number'] = $user['number'];
+    header("Location: ../index-j.php");
+
+} else {
+    echo '<p>ユーザー名またはパスワードが間違っています。</p>';
+    header("Location: ./login.php");
+}
+}
+?>
 
 
 ?>
 
 <h2>ログイン</h2>
-<form name="login" action="login.php" method="get">
+<form name="login" action="login.php" method="post">
     <p>UserId</p>
-    <input name="id" type="text">
+    <input name="number" type="text">
     <p>Password</p>
-    <input name="password" type="text"><br>
+    <input name="password" type="password"><br>
     <input type="submit" value="ログイン">
 </form>
