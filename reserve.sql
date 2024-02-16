57c4eec488dd61dcf5e91805df8d4a2342f295ad
KamiyamaKouki <92080227+KoukiFOL@users.noreply.github.com>
2024-02-16 07:37:45 +0000

    reserveデータベースの改良

--- reserve.sql
+++ reserve.sql
@@ -4,13 +4,29 @@ sqlite3 keionportal.db <reserve.sql
 sqlite> .read reserve.sql で実行
 */
 
-DROP TABLE reserves;
-CREATE TABLE reserves (
-    primarity INTEGER,
-    operation INTEGER,
-    grade INTEGER,
-    name TEXT,
-    time  DATETIME DEFAULT CURRENT_TIMESTAMP
+DROP TABLE IF EXISTS reserves;
+CREATE TABLE  reserves (
+    time TEXT, 
+    wednesday TEXT DEFAULT NULL,
+    thursday TEXT DEFAULT NULL,
+    friday TEXT DEFAULT NULL,
+    saturday TEXT DEFAULT NULL,
+    sunday TEXT DEFAULT NULL,
+    monday TEXT DEFAULT NULL,
+    tuesday TEXT DEFAULT NULL
     );
-INSERT INTO reserves(primary, operation, grade name) values(0, 1, 2, '上山航輝' );
+
+INSERT INTO reserves(time) values('8~9');
+INSERT INTO reserves(time) values('9~10');
+INSERT INTO reserves(time) values('10~11');
+INSERT INTO reserves(time) values('11~12');
+INSERT INTO reserves(time) values('12~13');
+INSERT INTO reserves(time) values('13~14');
+INSERT INTO reserves(time) values('14~15');
+INSERT INTO reserves(time) values('15~16');
+INSERT INTO reserves(time) values('16~17');
+INSERT INTO reserves(time) values('17~18');
+INSERT INTO reserves(time) values('18~19');
+INSERT INTO reserves(time) values('19~20');
+
 SELECT * FROM reserves;
